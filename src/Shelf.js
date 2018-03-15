136a90e19b5ef89f7d20712cd540d4dee6ed2454
jmp0xf <jmp0xf@gmail.com>
2018-03-15 23:01:06 +0800

    refactor: use stateless component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,44 +1,33 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 
-class Shelf extends Component {
-    static propTypes = {
-        title: PropTypes.string,
-        books: PropTypes.array.isRequired,
-        onMoveBook: PropTypes.func.isRequired
-    }
-
-    render() {
-        return (
-            <div className="bookshelf">
-                  {this.props.title?(<h2 className="bookshelf-title">{this.props.title}</h2>):null}
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books.map((book)=>(
-                            <li key={book.id}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+(book.imageLinks?book.imageLinks.smallThumbnail:"")+'")' }}></div>
-                                <div className="book-shelf-changer">
-                                  <select defaultValue={book.shelf} onChange={(event) => this.props.onMoveBook(book, event.target.value)}>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
-                            </div>
-                          </li>
-                        ))}
-                    </ol>
-                  </div>
-                </div>
-        )
-    }
+const Shelf = (props) => {
+    return <div className="bookshelf">
+            {props.title?(<h2 className="bookshelf-title">{props.title}</h2>):null}
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                  {props.books.map((book)=>(
+                      <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+(book.imageLinks?book.imageLinks.smallThumbnail:"")+'")' }}></div>
+                          <div className="book-shelf-changer">
+                            <select defaultValue={book.shelf} onChange={(event) => props.onMoveBook(book, event.target.value)}>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+                  ))}
+              </ol>
+            </div>
+          </div>
 }
 
 export default Shelf
