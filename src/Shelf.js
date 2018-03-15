4fdae555f0950dfcd7cbb1320ee2b1d77e495f6d
jmp0xf <jmp0xf@gmail.com>
2018-03-15 23:03:07 +0800

    fix: set a unique value to non-functional option to avoid collision

--- src/Shelf.js
+++ src/Shelf.js
@@ -12,7 +12,7 @@ const Shelf = (props) => {
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+(book.imageLinks?book.imageLinks.smallThumbnail:"")+'")' }}></div>
                           <div className="book-shelf-changer">
                             <select defaultValue={book.shelf} onChange={(event) => props.onMoveBook(book, event.target.value)}>
-                              <option value="none" disabled>Move to...</option>
+                              <option value="nan" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
                               <option value="read">Read</option>
