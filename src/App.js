b26878b4bf339cea99530eb705cc0e203fe6ece4
jmp0xf <jmp0xf@gmail.com>
2018-03-15 22:43:16 +0800

    refactor: init state within constructor

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Shelf from './Shelf'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    currentlyReadingBooks: [],
-    wantToReadBooks: [],
-    readBooks: [],
-    noneBooks: [],
-    searchedBooks: []
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      /**
+       * TODO: Instead of using this state variable to keep track of which page
+       * we're on, use the URL in the browser's address bar. This will ensure that
+       * users can use the browser's back and forward buttons to navigate between
+       * pages, as well as provide a good URL they can bookmark and share.
+       */
+      currentlyReadingBooks: [],
+      wantToReadBooks: [],
+      readBooks: [],
+      noneBooks: [],
+      searchedBooks: []
+    }
   }
 
   componentDidMount() {
